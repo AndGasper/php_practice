b55eb80520ff251f50540db19e3c19de82f6f880
AndGasper <andy.gasper1@gmail.com>
2017-07-05 12:24:49 -0700

    Initial commit + working through some basic OOP style PHP syntax.

---
+++ class_examples.php
@@ -0,0 +1,23 @@
+<?
+
+class SimpleClass {
+    
+    // property declaration 
+    public $var = 'Some value';
+    
+    // method declaration
+    public function displayVar() {
+        
+        echo $this->var; 
+    }
+}
+
+$simpleClassInstance = new SimpleClass(); 
+
+
+// class instance 
+// echo($simpleClassInstance->var);
+echo $simpleClassInstance->displayVar();
+ 
+
+?>
