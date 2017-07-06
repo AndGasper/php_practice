1095ce96820955d948ed76e5212f1eb3695d2524
AndGasper <andy.gasper1@gmail.com>
2017-07-06 08:51:49 -0700

    Commit of version containing notes on ready state changes of XMLHttpRequest

--- data_api/register_client.php
+++ data_api/register_client.php
@@ -1,6 +1,7 @@
 <?php
-echo var_dump($_POST);
-$output['debug'] = var_dump($_POST); 
+
+//$debugging = var_dump($_POST);
+$output['debug'] = $_POST; // Gets printed through the data.php print_r statement
 
 
 ?>
--- js/register.js
+++ js/register.js
@@ -1,8 +1,62 @@
 const BASE_URL = "data.php?action="
 function registerClient() {
-    const registerRequest = new XMLHttpRequest(); // make a new instance
-    registerRequest.open("POST", `${BASE_URL}register_client`); // *.open(METHOD, URL);
+    console.log("Register client function"); 
+    const registerRequest = new XMLHttpRequest(); // make a new instance; Should be ready state 0; 
+    
+    // Verification of XMLHttpRequest cleint's readyState(s) 
 
+    console.log(`Ready state AFTER new XMLHttpRequest() BUT BEFORE XMLHttpRequest.open(): ${registerRequest.readyState}`); 
+    // Output - Ready state AFTER new XMLHttpRequest() BUT BEFORE XMLHttpRequest.open(): 0
+    
+
+    registerRequest.open("POST", `${BASE_URL}register_client`); // XMLHttpRequest.open(METHOD, URL);
+    console.log(`Ready state AFTER XMLHttpRequest.open() BUT BEFORE XMLHttpRequest.send(): ${registerRequest.readyState}`); 
+    // Output - Ready state AFTER XMLHttpRequest.open() BUT BEFORE XMLHttpRequest.send(): 1
+
+    
+    registerRequest.send("Bleep bloop"); 
+    console.log(`Ready state AFTER XMLHttpRequest.send("Bleep bloop"): ${registerRequest.readyState}`);
+    // Output - Ready state AFTER XMLHttpRequest.send("Bleep bloop"): 1
+    
+    /**
+     * The Five (5) Steps to a Successful XMLHttpRequest
+     * 0. Open a request
+     * 1. Transmit the request
+     * 2. ???
+     * 3. Anticipate a response
+     * 4. Receive a response 
+     * 5. Profit!
+     */
+    
+    registerRequest.onprogress = function() {
+        console.log(`Ready state DURING the 'transaction' of a request: ${registerRequest.readyState}`); 
+        // Output - Ready state DURING the 'transaction' of a request: 3
+    };
+
+    registerRequest.onload = function() {
+        console.log(`Ready state AFTER the 'transaction' of a request: ${registerRequest.readyState}`); 
+        // Output - Ready state AFTER the 'transaction' of a request: 4
+    };
+
+    registerRequest.onreadystatechange = function() {
+        console.log("Inside XMLHttpRequest.onreadystatechange anonymous function"); 
+        if (registerRequest.readyState === 4) {
+            console.log(`registerRequest.response:, ${registerRequest.response}`); 
+            // Output - registerRequest.response:, {"debug":[]}
+        } else {
+            console.log(`Ready state while we wait: ${registerRequest.readyState}`);
+            console.log("I'm still waiting... \n-Crosseyed and Painless, Talking Heads");
+        };
+    }; 
+
+
+
+    // according to the header in Postman:
+    // "Content-Type -> text/html; charset=UTF-8"
+    // Put I could have sworn I set it to JSON...
+    // According to documentation, my options are "document" or an empty string, if I am reading the MDN documentation correctly
+    registerRequest.responseType = ''; // set the expected response type as JSON
+    
 
 }
 
@@ -21,3 +75,12 @@ $.ajax({
     
 });
 */
+
+/**
+ * Notes
+ * 0 - UNSENT: Client has been created. open() not called yet.
+ * 1 - OPENED: open() has been called.
+ * 2 - HEADERS_RECEIVED: send() has been called.
+ * 3 - LOADING: Downloading; responseText holds partial data.
+ * 4 - The operation is complete. 
+ */
