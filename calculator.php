8a9117c30b43fcd3d464ab18c67052850b5f1286
AndGasper <andy.gasper1@gmail.com>
2017-07-13 15:27:04 -0700

    Quick commit of calculator file

--- calculator.php
+++ calculator.php
@@ -9,15 +9,58 @@ $testInput = "2*2";
 
 function doMath($input) {
     $index = 0;
+    $operatorsArray = array(
+        "+" => "addition",
+        "-" => "subtraction"
+    );
     $inputLength = strlen($input); 
     while ($index < $inputLength) {
-        echo $input[$index]."<br/>";
+        //echo $input[$index]."<br/>";
+        switch(ord($input[$index])) {
+            // Addition (+) = 43 in ASCII
+            case(43):
+                $a = substr($input, 0,$index); // Grab the numbers from the start up to where the operator is
+                $b = substr($input,$index+1);
+                
+                return $a+$b;
+                break;
+
+            // Subtraction
+            case(45): 
+                $a = substr($input, 0,$index); // Grab the numbers from the start up to where the operator is
+                $b = substr($input,$index+1);
+                return $a-$b;
+                break;
+
+            // Division
+            case(47):
+                $a = substr($input, 0,$index); // Grab the numbers from the start up to where the operator is
+                $b = substr($input,$index+1);
+                return $a/$b;
+                break;
+
+            // Multiplication
+            case(42):
+                $a = substr($input, 0,$index); // Grab the numbers from the start up to where the operator is
+                $b = substr($input,$index+1);
+                return $a*$b;
+
+
+
+
+                
+        default:
+        echo '';
+        }
         $index++; 
     }
 }
 
-doMath("2+2"); 
-
+print("2+2=".doMath("2+2")."<br>");
+print("2-4=".doMath("2-4")."<br>");
+print("22+4=".doMath("22+4")."<br>");
+print("22-4=".doMath("22-4")."<br>");
+print("2*4=".doMath("2*4")."<br");
 ?> 
 
 <!DOCTYPE html> 
@@ -29,6 +72,12 @@ doMath("2+2");
         <p>This is my plastic fork</p>
         <p>Oven baking, heavy breathing</p>
     </div>
+
+    <form>
+        <button>Cut my life into pizza</button>
+        <button>This is my plastic fork</button>
+        <button>/</button>
+    </form>
 </body>
 </head>
 </html>
